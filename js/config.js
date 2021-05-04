6aabbaa06d80a87c01df5865ef86b546ecf5cab5
Gorschy <54874863+Gorschy@users.noreply.github.com>
2021-05-04 15:35:12 +1000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-southeast-2_TbLHZgnWd', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'kr4v606l9denrfeedtfhks6i5', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-southeast-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
