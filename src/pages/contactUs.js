275098dbec7d87410fed517245c78cf5f564c40a
Gorschy <54874863+Gorschy@users.noreply.github.com>
2021-07-22 18:58:36 +1000

    fixed scuffed indenting

--- src/pages/contactUs.js
+++ src/pages/contactUs.js
@@ -4,44 +4,33 @@ import image1 from '../assets/Nature1.png'
 import './contactUs.css';
 const { TextArea } = Input;
 const contactUs = () => {
-
     return (
         <div>    
             <Row>
                 <Col span={10}>
-                <Card title="Contact Details" bordered={true} style={{ width: 700 }}>
-      <p>Our Email Address: contact_email@lordofTrees.com.au</p>
-        
-      <p> Businesss Phone Line: #0474690899</p>
-       
-    </Card>  
-    <img className = "image1" src={image1} alt="asset"/>             
-     </Col>
-                <Col span={7}>              
-                <Card title="Submit Support Ticket" bordered={false} style={{ width: 700 }}>
-      <form>
-       <Form.Item name="input" label="Email" rules={[{ required: true }]}>
-                      <Input placeholder="Enter Your Email"/>
-                  </Form.Item>
-                    </form>  
-                    <form>
-       <Form.Item name="input" label="Name" rules={[{ required: true }]}>
-                      <Input placeholder="Enter Your Name"/>
-                  </Form.Item>
-                    </form>    
-                    
-                    <TextArea rows={8} />
-                
-                    <Button className="submitButton">Submit</Button>   
-
-
-    </Card>
+                    <Card title="Contact Details" bordered={true} style={{ width: 700 }}>
+                    <p>Our Email Address: contact_email@lordofTrees.com.au</p>
+                    <p> Businesss Phone Line: #0474690899</p>
+                    </Card>  
+                    <img className = "image1" src={image1} alt="asset"/>             
                 </Col>
-     
-            </Row>
-
-               
-
+                <Col span={7}>              
+                    <Card title="Submit Support Ticket" bordered={false} style={{ width: 700 }}>
+                        <form>
+                            <Form.Item name="input" label="Email" rules={[{ required: true }]}>
+                            <Input placeholder="Enter Your Email"/>
+                            </Form.Item>
+                        </form>  
+                        <form>
+                            <Form.Item name="input" label="Name" rules={[{ required: true }]}>
+                            <Input placeholder="Enter Your Name"/>
+                            </Form.Item>
+                        </form>     
+                        <TextArea rows={8} />
+                        <Button className="submitButton">Submit</Button>   
+                    </Card>
+                </Col>
+             </Row>
         </div>
     );
 }
