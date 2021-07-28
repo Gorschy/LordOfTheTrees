bf4201b4b2ab9663fdc746782b42ba01f8482052
Gorschy <54874863+Gorschy@users.noreply.github.com>
2021-07-29 00:36:33 +1000

    Login Auth Change
    Authing on our end instead of aws amplify

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-import Amplify from 'aws-amplify';
+import Amplify, { Auth } from 'aws-amplify';
 import awsconfig from './aws-exports';
 import { AmplifySignOut, withAuthenticator } from '@aws-amplify/ui-react';
 import Navbar from './components/Navbar/Navbar';
 import { Layout } from 'antd';
-import { Route, Switch, Redirect } from 'react-router-dom';
+import { Route, Switch, Redirect, Link } from 'react-router-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import home from './pages/home';
 import calculator from './pages/calculator';
 import contactUs from './pages/contactUs';
 import register from './pages/register';
-import login from './pages/login';
+import SignIn from './pages/login';
 import dashboard from './pages/dashboard';
 import account from './pages/account';
-
+import { Button } from 'antd';
 
 
 
@@ -23,6 +23,32 @@ const { Header, Content } = Layout;
 Amplify.configure(awsconfig);
 
 function App() {
+
+    const[loggedIn, setLoggedIn] = useState(false);
+
+    const assessLoggedInState = () => {
+        Auth.currentAuthenticatedUser().then(sess => {
+            console.log('logged in');
+            setLoggedIn(true);
+        }).catch(() => {
+            console.log('not logged in');
+            setLoggedIn('false');
+        });
+    };
+
+    useEffect(() => {
+        assessLoggedInState();
+    }, []);
+
+    const signOut = async () => {
+        try {
+            await Auth.signOut();
+            setLoggedIn(false);
+        } catch (error) {
+            console.log('error signing out: ', error);
+        }
+    };
+
     return (
         <div>
             <Router>
@@ -34,7 +60,18 @@ function App() {
                     
                     <Content className="contentContainer">
                         <div>
-                            <AmplifySignOut />
+                           { loggedIn ? (
+                                <Button onClick={signOut} type="primary">
+                                    Sign Out
+                                    
+                                </Button> 
+                            ) : ( 
+                                <Link to="/login">    
+                                    <Button type="primary">
+                                        Log In
+                                    </Button>
+                                </Link>
+                            )}
                             
                             <Switch>
                                 <Route exact path="/home" component={home} />
@@ -44,7 +81,9 @@ function App() {
                                 <Route exact path="/calculator" component={calculator} />
                                 <Route exact path="/contactUs" component={contactUs} />
                                 <Route exact path="/register" component={register} />
-                                <Route exact path="/login" component={login} />
+                                <Route exact path="/login"> 
+                                    <SignIn onSignin={assessLoggedInState} />     
+                                </Route>
                                 <Route exact path="/dashboard" component={dashboard} />
                                 <Route exact path="/account" component={account} />
                             </Switch>
@@ -59,4 +98,4 @@ function App() {
     );
 }
 
-export default withAuthenticator(App);
+export default App;
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,71 +1,48 @@
+import React, { useState } from 'react';
+import { Auth } from 'aws-amplify';
 import { Form, Input, Button, Checkbox, Card } from 'antd';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
-const login = () => {
+const Signin = ({ onSignin }) => {
 
-    const onFinish = (values) => {
-        console.log('Success:', values);
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const history = useHistory();
+
+
+    const signIn = async () => {
+        try {
+            const user = await Auth.signIn(username, password);
+            history.push('/');
+            onSignin();
+
+        } catch (error) {
+            console.log('error singing in', error);
+        }
     };
-    
-    const onFinishFailed = (errorInfo) => {
-        console.log('Failed:', errorInfo);
-    };
+
 
     return (  
           
         <div>
-            <Card title="Login">
-        
-                <Form
-                    name="login"
-                    layout="vertical"
-                    requiredMark={'optional'}
-                    initialValues={{ remember: true }}
-                    onFinish={onFinish}
-                    onFinishFailed={onFinishFailed}
-                    scrollToFirstError
-                >
-                    <Form.Item
-                        label="Email"
-                        name="email"
-                        rules={[
-                            { 
-                                required: true, message: 'Please enter your Email Address!' 
-                            },
-                            {
-                                type: 'email',
-                                message: 'The input is not valid E-mail!',
-                            },
-                        ]}
-                    >
-                        <Input placeholder="Enter your Email" />
-                    </Form.Item>
+            <Input 
+                id="username"
+                label="Username" 
+                value={username}
+                onChange={e => setUsername(e.target.value)}
+            />
+            <Input 
+                id="password"
+                label="Password" 
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+            />
 
-                    <Form.Item
-                        label="Password"
-                        name="password"
-                        rules={[{ required: true, message: 'Please input your password!' }]}
-                    >
-                        <Input.Password placeholder="Enter your Password"/>
-                    </Form.Item>
-
-                    <Form.Item name="remember" valuePropName="checked">
-                        <Checkbox>Remember me</Checkbox>
-                        <a href="https://lordofthetrees.org/">Forgot Password?</a>
-                    </Form.Item>
-
-                    <Form.Item>
-                        <Button type="primary" htmlType="submit">
-                            Login
-                        </Button>
-                    </Form.Item>
-                    <Form.Item>
-                        Don't have an account? <Link to='/register'>Sign Up!</Link>
-                    </Form.Item>
-                </Form>
-            </Card>                
+            <Button id="signinButton" onClick={signIn} type="primary">
+                Sign In                  
+            </Button> 
         </div>
     );
 }
 
-export default login;
+export default Signin;
