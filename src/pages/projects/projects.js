3e9c0cd9308e808040c0fe7c144f39ffbb6447a9
brandonBritton <brandon.britton09@gmail.com>
2021-09-12 16:22:09 +1000

    fix

--- src/pages/projects/projects.js
+++ src/pages/projects/projects.js
@@ -1,24 +1,36 @@
-import React, { useContext } from 'react';
 import '../../style.css';
 import './projects.css';
+import React, { useContext, useEffect } from 'react';
+import { Card } from 'antd';
 import { UserContext } from '../UserContext';
 import { Redirect } from 'react-router'
+import { Auth } from 'aws-amplify';
 
-
-function Projects() { 
+const Projects = () => { 
     
     const {loggedIn} = useContext(UserContext);
 
+    useEffect(() => {
+        displayUserDetails();
+    }, []);
+
+    const displayUserDetails = async () => {
+        //Capture current authed user
+        let user = await Auth.currentAuthenticatedUser();
+        const { attributes } = user;
+        console.log(attributes);
+
+        
+    }
+    
+
     /* ------ Mock Data ------ */
 
-    const testUser = {
-
-    }
-
+    
     const projects = {
         id: 1,
         name: 'Test Project',
-
+        description: 'Test Description'
     }
 
     /* ----------------------- */
@@ -26,19 +38,46 @@ function Projects() {
     return (
     
         <div>
-            
-        { loggedIn 
-        ? (
-        
-            <div></div>
+            { 
+                loggedIn ? (
+                
+                    <div className='container'>
+                        <div className='column'>
+                            
+                            <div id='projectListCard'>
+                                <h3 className='centerContent'>Projects</h3>
+                            </div>
 
-        ) 
-        
-        : (
-            <Redirect to="/home"/>
-        ) 
-        }
-        
+                            <div id='carbonReportListCard'>
+                                <h3 className='centerContent'>Carbon Reports</h3>
+                            </div>
+                            
+                        </div>
+                        <div className='projectSummary'>
+                            <div className='projectAnalytics'>
+                                <h3 className='centerContent'>Project Summary</h3>
+                            </div>
+                            <div className='projectBreakdown'>
+                                <h3 className='leftContent'>Project Summary</h3>
+                            </div>
+                        </div>
+                        <div className='column'>
+                            <Card
+                                id='projectAdminCard'
+                                bordered={false}
+                                title={<h3 id="centreContent">Project Admin</h3>}
+                            />
+                        
+                            <Card
+                                id='membersList'
+                                bordered={false}
+                                title={<h3 id="centreContent">Team Members</h3>}
+                            />
+                        </div>
+                    </div>
+
+                ) : ( <Redirect to="/home"/> ) 
+            } 
         </div>
         
     );
